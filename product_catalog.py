84c386f9544cb68df672316b26d2fcf620d8c07a
Criss <qualyn.criss@mymail.lourdes.edu@Mac.attlocal.net>
2025-09-01 22:28:36 -0400

    Final version: product recommendation function and design memo

--- product_catalog.py
+++ product_catalog.py
@@ -1,62 +1,68 @@
 from product_data import products
 # TODO: Step 1 - Print out the products to see the data that you are working with.
+products = [
+    {"name": "Eco Water Bottle", "tags": ["eco-friendly", "durable", "recyclable"]},
+    {"name": "Trail Backpack", "tags": ["durable", "water-resistant", "lightweight"]},
+    {"name": "Vegan Leather Wallet", "tags": ["vegan", "stylish", "compact"]},
+    {"name": "Bamboo Toothbrush", "tags": ["eco-friendly", "vegan", "biodegradable"]},
+    {"name": "Smartwatch", "tags": ["tech", "durable", "stylish"]},
+]
 
+print("Products available:")
+for product in products:
+    print(product)
 
 
 # TODO: Step 2 - Create a list called customer_preferences and store the user preference in this list.
+customer_preferences = []
+
+while True:
+    preference = input("Input a preference: ")
+    customer_preferences.append(preference)
+    
+    response = input("Do you want to add another preference? (Y/N): ").strip().upper()
+    if response == "N":
+        break
 
 
-response = ""
-while response != "N":
-    print("Input a preference:")
-    preference = input()
-    # Add the customer preference to the list
-
-    response = input("Do you want to add another preference? (Y/N): ").upper()
-  
-
-# TODO: Step 3 - Convert customer_preferences list to set to eliminate duplicates.
+# TODO: Step 3 - Convert customer_preferences list to set to eliminate duplicates
+customer_tags = set(customer_preferences)
 
 
-
-# TODO: Step 4 - Convert the product tags to sets in order to allow for faster comparisons.
+# TODO: Step 4 - Convert the product tags to sets in order to allow for faster comparisons
 converted_products = []
-
-
+for product in products:
+    converted_products.append({"name": product["name"], "tags": set(product["tags"])})
 
 
 # TODO: Step 5 - Write a function to calculate the number of matching tags
 def count_matches(product_tags, customer_tags):
-    '''
-    Args:
-        product_tags (set): A set of tags associated with a product.
-        customer_tags (set): A set of tags associated with the customer.
-    Returns:
-        int: The number of matching tags between the product and customer.
-    '''
-    pass
-
-
+    return len(product_tags.intersection(customer_tags))
 
 
 # TODO: Step 6 - Write a function that loops over all products and returns a sorted list of matches
 def recommend_products(products, customer_tags):
-    '''
-    Args:
-        products (list): A list of product dictionaries.
-        customer_tags (set): A set of tags associated with the customer.
-    Returns:
-        list: A list of products containing product names and their match counts.
-    '''
-    pass
-
+    recommendations = []
+    for product in products:
+        matches = count_matches(product["tags"], customer_tags)
+        if matches > 0:
+            recommendations.append({"name": product["name"], "matches": matches})
+    return sorted(recommendations, key=lambda x: x["matches"], reverse=True)
 
 
 # TODO: Step 7 - Call your function and print the results
+results = recommend_products(converted_products, customer_tags)
+print("\nRecommended products based on your preferences:")
+for item in results:
+    print(f"{item['name']} ({item['matches']} match(es))")
 
 
-
-
-# DESIGN MEMO (write below in a comment):
+# DESIGN MEMO
 # 1. What core operations did you use (e.g., intersections, loops)? Why?
+#    - I used loops to iterate over each product and the set intersection operation to find the number
+#      of matching tags between customer preferences and product tags. Sets were used to avoid duplicates
+#      and make membership checks fast.
 # 2. How might this code change if you had 1000+ products?
+#    - For large datasets, iterating over every product could become slow. Optimizations might include
+#      indexing products by tags in a dictionary, using a database, or implementing more advanced
+#      recommendation algorithms to handle bigger catalogs efficiently.
